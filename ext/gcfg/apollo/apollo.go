6e4206f0ee50af9e80223995d6c21bc5a175c324
CharLemAznable <545541819@qq.com>
2024-07-09 16:58:40 +0800

    Some refactor.

--- ext/gcfg/apollo/apollo.go
+++ ext/gcfg/apollo/apollo.go
@@ -4,10 +4,20 @@ import (
 	"context"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gcfg"
+	"github.com/gogf/gf/v2/os/gcmd"
+	"os"
+	"strings"
 )
 
 const (
 	defaultApolloConfigFileName = "apollo"
+	apolloConfigKey             = "apollo"
+
+	apolloAppIdPattern     = "gf.gcfg.apollo.appid"
+	apolloIPPattern        = "gf.gcfg.apollo.ip"
+	apolloClusterPattern   = "gf.gcfg.apollo.cluster"
+	apolloNamespacePattern = "gf.gcfg.apollo.namespace"
+	apolloKeyPattern       = "gf.gcfg.apollo.key"
 )
 
 func LoadAdapter(ctx context.Context, fileName ...string) (gcfg.Adapter, error) {
@@ -15,16 +25,35 @@ func LoadAdapter(ctx context.Context, fileName ...string) (gcfg.Adapter, error) 
 	if len(fileName) > 0 && fileName[0] != "" {
 		apolloConfigFileName = fileName[0]
 	}
-	apolloCfg := g.Cfg(apolloConfigFileName)
-	apolloCfgData, err := apolloCfg.Data(ctx)
-	if err != nil {
-		return nil, err
-	}
-	apolloCfgVar := g.NewVar(apolloCfgData, true)
+	apolloCfg, _ := g.Cfg(apolloConfigFileName).Get(ctx, apolloConfigKey)
 	apolloConfig := Config{}
-	err = apolloCfgVar.Struct(&apolloConfig)
-	if err != nil {
-		return nil, err
+	_ = apolloCfg.Struct(&apolloConfig)
+
+	if apolloConfig.AppID == "" {
+		apolloConfig.AppID = getOptWithEnv(apolloAppIdPattern)
+	}
+	if apolloConfig.IP == "" {
+		apolloConfig.IP = getOptWithEnv(apolloIPPattern)
+	}
+	if apolloConfig.Cluster == "" {
+		apolloConfig.Cluster = getOptWithEnv(apolloClusterPattern)
+	}
+	if apolloConfig.NamespaceName == "" {
+		apolloConfig.NamespaceName = getOptWithEnv(apolloNamespacePattern)
+	}
+	if apolloConfig.Key == "" {
+		apolloConfig.Key = getOptWithEnv(apolloKeyPattern)
 	}
 	return NewAdapterApollo(ctx, apolloConfig)
+}
+
+func getOptWithEnv(key string) string {
+	if v := gcmd.GetOpt(key); !v.IsEmpty() {
+		return v.String()
+	}
+	envKey := strings.ToUpper(strings.ReplaceAll(key, ".", "_"))
+	if r, ok := os.LookupEnv(envKey); ok && r != "" {
+		return r
+	}
+	return ""
 }
--- ext/gcfg/apollo/apollo_z_unit_test.go
+++ ext/gcfg/apollo/apollo_z_unit_test.go
@@ -3,23 +3,46 @@ package apollo_test
 import (
 	"context"
 	"github.com/CharLemAznable/gfx/ext/gcfg/apollo"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gcfg"
+	"github.com/gogf/gf/v2/os/gcmd"
+	"github.com/gogf/gf/v2/os/genv"
 	"github.com/gogf/gf/v2/test/gtest"
 	"testing"
 )
 
+var (
+	ctx = context.TODO()
+)
+
 func Test_New_Default(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
-		ctx := context.TODO()
 		adapter, err := apollo.LoadAdapter(ctx)
 		t.AssertNE(adapter, nil)
 		t.AssertNil(err)
+
+		config := gcfg.NewWithAdapter(adapter)
+		value, err := config.Get(ctx, "key")
+		g.Dump(value, err)
 	})
 }
 
 func Test_New_Error(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
-		ctx := context.TODO()
 		adapter, err := apollo.LoadAdapter(ctx, "apollo_error")
+		t.AssertNil(adapter)
+		t.AssertNE(err, nil)
+		t.Assert(err.Error(), "The Key field is required")
+	})
+}
+
+func Test_New_None(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		_ = genv.Set("GF_GCFG_APOLLO_APPID", "APPID")
+		defer func() { _ = genv.Remove("GF_GCFG_APOLLO_APPID") }()
+		gcmd.Init([]string{"--gf.gcfg.apollo.ip=IP"}...)
+
+		adapter, err := apollo.LoadAdapter(ctx, "apollo_none")
 		t.AssertNil(adapter)
 		t.AssertNE(err, nil)
 		t.Assert(err.Error(), "The Key field is required")
